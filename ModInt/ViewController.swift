4413d48c12cc9b3a915d5c2b9dc04ddf05da0528
Dengfeng Zhang <arondes@gmail.com>
2016-02-17 23:05:09 -0500

    hahahah

--- ModInt/ViewController.swift
+++ ModInt/ViewController.swift
@@ -10,17 +10,28 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var LBL: UILabel!
+    @IBOutlet weak var calc: UIButton!
+    @IBOutlet weak var x: UITextField!
+    @IBOutlet weak var y: UITextField!
+    @IBOutlet weak var res: UILabel!
+    @IBOutlet weak var p: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        LBL.text="Starting...\n"
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func click(sender: UIButton) {
+        let a = Double((x.text! as NSString).doubleValue)
+        let b = Double((y.text! as NSString).doubleValue)
+        let c = Double((p.text! as NSString).doubleValue)
+        let first=ModInt(Int(a),Int(c))
+        let second=ModInt(Int(b),Int(c))
+        let result=first+second
+        res.text="= "+String(result.getv())
+    }
 
 }
-
