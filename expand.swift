61ab08cc7235e3a8df1b1ff5c87b82b9b714f874
arondes <arondes@gmail.com>
2016-02-19 16:03:13 -0500

    Update expand.swift

--- expand.swift
+++ expand.swift
@@ -2,7 +2,7 @@ import Foundation
 func somefunc(x: Double) -> Double?{
     var result: Double?
     if(x<=10.0 && x>=0.0){
-        result=sin(x)
+        result=x*2
     }
     else{
         result=nil
@@ -13,12 +13,24 @@ func somefunc(x: Double) -> Double?{
 func expand(f:(Double)->(Double?),_ left:Double,_ right:Double)->(Double)->(Double?){
     func newf(newx: Double)-> Double{
         let period=right-left
-        let realx=newx-Double(Int(newx/period))*period
+        if(period<=0.0){
+            return 0.0
+        }
+        var realx: Double=0.0
+        if(newx>right){
+            realx=newx-Double(Int(newx/period))*period
+        }
+        else if(newx<left){
+            realx=newx-Double(Int(newx/period)-1)*period
+        }
+        else {
+            realx=newx
+        }
         if(f(realx)==nil){
-            return 0
+            return 0.0
         }
         return f(realx)!
     }
     return newf
 }
-print(expand(somefunc,0.0,10.0)(19))
+print(expand(somefunc,2,8)(-5))
